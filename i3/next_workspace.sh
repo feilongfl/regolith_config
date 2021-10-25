08a9115c928d738cf0cb6e11a6b6e79188749313
YuLong Yao <feilongphone@gmail.com>
2021-10-25 20:18:27 +0800

    change workspace next/prev command

---
+++ i3/next_workspace.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env fish
+
+set primary_screen (i3-msg -t get_config | grep 'set $primary_screen' | cut -d ' ' -f 3)
+set maxWorkspaceNum (i3-msg -t get_workspaces | jq '.[] | select(.output=="'$primary_screen'") | .num' | sort -n | tail -n1)
+set focusWorkspaceNum (i3-msg -t get_workspaces | jq '.[] | select(.focused==true) | .num' | sort -n | tail -n1)
+
+echo $maxWorkspaceNum $focusWorkspaceNum
+if test $maxWorkspaceNum -eq $focusWorkspaceNum
+    set nextWorkspaceNum (expr $maxWorkspaceNum + 1)
+    echo maxWorkspaceNum $maxWorkspaceNum
+    echo nextWorkspaceNum $nextWorkspaceNum
+
+    i3-msg workspace number $nextWorkspaceNum
+else
+    set nextWorkspaceNum (expr $focusWorkspaceNum + 1)
+    echo focusWorkspaceNum $focusWorkspaceNum
+    echo nextWorkspaceNum $nextWorkspaceNum
+
+    if test $nextWorkspaceNum -lt 10
+        i3-msg workspace number $nextWorkspaceNum
+    else
+        # back to first
+        i3-msg workspace number 1
+    end
+end
