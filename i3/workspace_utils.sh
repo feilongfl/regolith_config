0ab192dfd0cc3643a08f9e3e5a20f5ef4efecc15
YuLong Yao <feilongphone@gmail.com>
2021-10-26 06:27:01 +0800

    Revert "load var by direnv"
    This reverts commit df282449ca623536c925a9f391dd7f7f44052e5c.

--- i3/workspace_utils.sh
+++ i3/workspace_utils.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env fish
 
-# enable direnv
-direnv hook fish | source
-
-# load direnv
-direnv allow (dirname (status --current-filename))
-echo test $primary_screen_max_workspace
-
 function toWorkspace
     i3-msg workspace number $argv
 end
@@ -42,7 +35,7 @@ function next_workspace
         echo maxWorkspaceNum $maxWorkspaceNum
         echo nextWorkspaceNum $nextWorkspaceNum
 
-        if test $nextWorkspaceNum -lt $primary_screen_max_workspace
+        if test $nextWorkspaceNum -lt 10
             eval $argv $nextWorkspaceNum
         else
             # back to first
