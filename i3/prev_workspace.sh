a309a8f685b892056b572dd48de29d77c0567c20
YuLong Yao <feilongphone@gmail.com>
2021-10-25 20:38:09 +0800

    Refactor workspace script

--- i3/prev_workspace.sh
+++ i3/prev_workspace.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env fish
 
-set primary_screen (i3-msg -t get_config | grep 'set $primary_screen' | cut -d ' ' -f 3)
-set minWorkspaceNum (i3-msg -t get_workspaces | jq '.[] | select(.output=="'$primary_screen'") | .num' | sort -n | head -n1)
-set maxWorkspaceNum (i3-msg -t get_workspaces | jq '.[] | select(.output=="'$primary_screen'") | .num' | sort -n | tail -n1)
-set focusWorkspaceNum (i3-msg -t get_workspaces | jq '.[] | select(.focused==true) | .num' | sort -n | tail -n1)
+source (dirname (status --current-filename))/workspace_utils.sh
 
-echo $minWorkspaceNum $focusWorkspaceNum
-if test $minWorkspaceNum -eq $focusWorkspaceNum
-    # to end
-    i3-msg workspace number $maxWorkspaceNum
-else
-    set prevWorkspaceNum (expr $focusWorkspaceNum - 1)
-    echo focusWorkspaceNum $focusWorkspaceNum
-    echo prevWorkspaceNum $prevWorkspaceNum
-
-    i3-msg workspace number $prevWorkspaceNum
-end
+prev_workspace toWorkspace
---
+++ i3/workspace_utils.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env fish
+
+function toWorkspace
+    i3-msg workspace number $argv
+end
+
+set primary_screen (i3-msg -t get_config | grep 'set $primary_screen' | cut -d ' ' -f 3)
+set minWorkspaceNum (i3-msg -t get_workspaces | jq '.[] | select(.output=="'$primary_screen'") | .num' | sort -n | head -n1)
+set maxWorkspaceNum (i3-msg -t get_workspaces | jq '.[] | select(.output=="'$primary_screen'") | .num' | sort -n | tail -n1)
+set focusWorkspaceNum (i3-msg -t get_workspaces | jq '.[] | select(.focused==true) | .num' | sort -n | tail -n1)
+
+function prev_workspace
+    echo $minWorkspaceNum $focusWorkspaceNum
+    if test $minWorkspaceNum -eq $focusWorkspaceNum
+        # to end
+        eval $argv $maxWorkspaceNum
+    else
+        set prevWorkspaceNum (expr $focusWorkspaceNum - 1)
+        echo focusWorkspaceNum $focusWorkspaceNum
+        echo prevWorkspaceNum $prevWorkspaceNum
+
+        eval $argv $prevWorkspaceNum
+    end
+end
+
+function next_workspace
+    echo $maxWorkspaceNum $focusWorkspaceNum
+    if test $maxWorkspaceNum -eq $focusWorkspaceNum
+        set nextWorkspaceNum (expr $maxWorkspaceNum + 1)
+        echo maxWorkspaceNum $maxWorkspaceNum
+        echo nextWorkspaceNum $nextWorkspaceNum
+
+        if test $nextWorkspaceNum -lt 10
+            toWorkspace $nextWorkspaceNum
+        else
+            # back to first
+            toWorkspace 1
+        end
+    else
+        set nextWorkspaceNum (expr $focusWorkspaceNum + 1)
+        echo focusWorkspaceNum $focusWorkspaceNum
+        echo nextWorkspaceNum $nextWorkspaceNum
+
+        toWorkspace $nextWorkspaceNum
+    end
+end
